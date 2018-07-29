8c8ca873cf17fd175aed6b47b1e70b0b980a3de5
Sven Mawby <fr34kyn01535@bam.yt>
2018-07-29 12:10:11 +0200

    ..

--- LMPatcher/Program.cs
+++ LMPatcher/Program.cs
@@ -16,57 +16,70 @@ namespace LMPatcher
         {
             string path = args[0];
 
-            ZipArchive archive = ZipFile.Open(path, ZipArchiveMode.Update);
-            ZipArchiveEntry assemblyEntry = archive.GetEntry("assets/bin/Data/Managed/Assembly-CSharp.dll");
-            archive.GetEntry("assets/bin/Data/Managed/UnityEngine.dll").ExtractToFile("UnityEngine.dll");
-            byte[] assemblyBytes;
-            using (var ms = new MemoryStream())
+            using (ZipArchive archive = ZipFile.Open(path, ZipArchiveMode.Update))
             {
-                assemblyEntry.Open().CopyTo(ms);
-                assemblyBytes = ms.ToArray();
-            }
-            byte[] password = assemblyBytes.Skip(32).Take(8).ToArray();
-            int passwordIndex = 0;
-            for (int i = 0; i < assemblyBytes.Length; i++)
-            {
-                if (passwordIndex == 8) passwordIndex = 0;
-                assemblyBytes[i] ^= password[passwordIndex++];
-            }
-
-
-            AssemblyDefinition assembly = AssemblyDefinition.ReadAssembly(new MemoryStream(assemblyBytes));
-
-
-            foreach (var patch in Assembly.GetExecutingAssembly().GetTypes().Where(t => t.BaseType.FullName == "LMPatcher.Patch").Select(t => Activator.CreateInstance(t) as Patch))
-            {
-                try
+                ZipArchiveEntry assemblyEntry = archive.GetEntry("assets/bin/Data/Managed/Assembly-CSharp.dll");
+                archive.GetEntry("assets/bin/Data/Managed/UnityEngine.dll").ExtractToFile("UnityEngine.dll", true);
+                foreach (string entry in archive.Entries.Where(e => e.FullName.StartsWith("META-INF")).Select(e => e.FullName).ToList())
                 {
-                    patch.Assembly = assembly;
-                    patch.Apply();
+                    archive.GetEntry(entry).Delete();
                 }
-                catch (Exception ex)
+                byte[] assemblyBytes;
+                using (var stream = new MemoryStream())
                 {
-                    Console.WriteLine("Error in " + patch.GetType().Name + ":" + ex.ToString());
+                    assemblyEntry.Open().CopyTo(stream);
+                    assemblyBytes = stream.ToArray();
+
+                    byte[] password = assemblyBytes.Skip(32).Take(8).ToArray();
+                    int passwordIndex = 0;
+                    for (int i = 0; i < assemblyBytes.Length; i++)
+                    {
+                        if (passwordIndex == 8) passwordIndex = 0;
+                        assemblyBytes[i] ^= password[passwordIndex++];
+                    }
+
+
+                    AssemblyDefinition assembly = AssemblyDefinition.ReadAssembly(new MemoryStream(assemblyBytes));
+
+
+                    foreach (var patch in Assembly.GetExecutingAssembly().GetTypes().Where(t => t.BaseType.FullName == "LMPatcher.Patch").Select(t => Activator.CreateInstance(t) as Patch))
+                    {
+                        try
+                        {
+                            patch.Assembly = assembly;
+                            patch.Apply();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Error in " + patch.GetType().Name + ":" + ex.ToString());
 #if DEBUG
-                    Console.ReadLine();
+                            Console.ReadLine();
 #endif
-                    Environment.Exit(1);
+                            Environment.Exit(1);
+                        }
+                    }
+
+                    using (var ms = new MemoryStream())
+                    {
+                        assembly.Write(ms);
+                        assemblyBytes = ms.ToArray();
+                    }
+
+                    File.WriteAllBytes("Assembly-CSharp.patched.dll", assemblyBytes);
+
+                    passwordIndex = 0;
+                    for (int i = 0; i < assemblyBytes.Length; i++)
+                    {
+                        if (passwordIndex == 8) passwordIndex = 0;
+                        assemblyBytes[i] ^= password[passwordIndex++];
+                    }
+
+                    stream.SetLength(assemblyBytes.Length);
+                    using (StreamWriter writer = new StreamWriter(stream))
+                    {
+                        writer.Write(assemblyBytes);
+                    }
                 }
-            }
-
-            using (var ms = new MemoryStream())
-            {
-                assembly.Write(ms);
-                assemblyBytes = ms.ToArray();
-            }
-
-            File.WriteAllBytes("Assembly-CSharp.patched.dll", assemblyBytes);
-
-            passwordIndex = 0;
-            for (int i = 0; i < assemblyBytes.Length; i++)
-            {
-                if (passwordIndex == 8) passwordIndex = 0;
-                assemblyBytes[i] ^= password[passwordIndex++];
             }
         }
     }
